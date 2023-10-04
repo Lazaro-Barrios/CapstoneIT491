cc082d17ffc4f78603d33858689e8d762f415775
billyr06 <billyramirez06@yahoo.com>
2023-10-03 20:25:04 -0400

    the user can now multi-select in the Level of Care dropbox

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,14 +1,55 @@
-const menuBtn = document.querySelector('.menu-btn');
-const dropdown = document.querySelector('.dropdown-content');
-const closeMenu = document.getElementById('closeMenu');
+document.addEventListener("DOMContentLoaded", function() {
+    // Dropdown Menu Toggle
+    const menuBtn = document.querySelector('.menu-btn');
+    const dropdown = document.querySelector('.dropdown-content');
+    const closeMenu = document.getElementById('closeMenu');
 
-menuBtn.addEventListener('click', (event) => {
-    if (event.target !== closeMenu) {
+    menuBtn.addEventListener('click', () => {
         dropdown.classList.toggle('show');
-    }
-});
+    });
 
-closeMenu.addEventListener('click', (event) => {
-    dropdown.classList.remove('show');
-    event.stopPropagation(); // Prevent the event from bubbling up to the .menu-btn
+    closeMenu.addEventListener('click', (event) => {
+        dropdown.classList.remove('show');
+        event.stopPropagation(); // Prevent event from bubbling up to menuBtn
+    });
+
+    const $anotherFilter = $('.another-filter');
+
+    $anotherFilter.select2({
+        placeholder: "",  // Placeholder text
+        allowClear: true,  // Enables the ability to clear the selection
+        minimumResultsForSearch: Infinity,  // Disables the search box
+        templateSelection: data => data.text
+    }).on('select2:select select2:unselect', function (e) {
+        // Handle 'Select All' functionality
+        if (e.params.data.id === 'selectAll') {
+            $(this).val(
+                e.type === 'select2:select' 
+                ? $(this).find('option').map(function() { 
+                    return $(this).val() !== 'selectAll' ? $(this).val() : null; 
+                  }).get()
+                : []
+            ).trigger('change');
+        }
+        updatePlaceholder($anotherFilter);
+    }).on('select2:open', function() {
+        // Add click event listener to document when dropdown is open
+        $(document).on('click.select2', function (e) {
+            if (!$(e.target).closest('.select2').length) {
+                $anotherFilter.select2('close');
+            }
+        });
+    }).on('select2:close', function() {
+        // Remove click event listener when dropdown is closed
+        $(document).off('click.select2');
+    });
+
+    function updatePlaceholder($selectElement) {
+        // Manually insert a placeholder
+        if ($selectElement.val().length === 0) {
+            $(".select2-selection__rendered").attr("title","Level of Care").text("Level of Care");
+        }
+    }
+
+    updatePlaceholder($anotherFilter);
 });
--- Pages/programPayments.cfm
+++ Pages/programPayments.cfm
@@ -3,6 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- jQuery -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/css/select2.min.css" rel="stylesheet" />
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/js/select2.min.js"></script>
+
     <link rel="stylesheet" href="/CapstoneIT491/CSS/styles.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <title>Program Payments</title>
@@ -12,6 +17,7 @@
 
     <h1>Program Payments</h1>
     <select class="year-filter">
+        <option value="" disabled selected>Year</option>
         <option value="2015">2015</option>
         <option value="2016">2016</option>
         <option value="2017">2017</option>
@@ -20,7 +26,8 @@
         <option value="2020">2020</option>
         <option value="2021">2021</option>
     </select>
-    <select class="another-filter">
+    <select class="another-filter" multiple="multiple">
+        <option value="selectAll">Select All</option>
         <option value="option1">Routine Home Care</option>
         <option value="option2">Continuous Home Care</option>
         <option value="option3">Inpatient Home Care</option>
