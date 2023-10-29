65e65f0599e63086b48cde65bfa76f7b6458ffdb
Gabriel Eaton <50244998+ge64@users.noreply.github.com>
2023-10-28 21:20:14 -0400

    Modal chart working with dummy data

--- JavaScript/spending.js
+++ JavaScript/spending.js
@@ -1,6 +1,50 @@
 $(document).ready(function() {
     let dataTable;
 
+    function renderChart(brandName) {
+        // Sample static data, you might want to fetch real data for the brandName clicked
+        const data = {
+            labels: ['2017', '2018', '2019', '2020', '2021'],
+            datasets: [{
+                label: brandName,
+                data: [12, 19, 3, 5, 2], // replace with real data
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1
+            }]
+        };
+
+        // Configurations for the Chart.js chart
+        const config = {
+            type: 'line',
+            data: data,
+            options: {
+                responsive: true,
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        };
+
+        // Destroy any previously created chart to avoid overlapping
+        if (window.myChart) {
+            window.myChart.destroy();
+        }
+
+        const ctx = document.getElementById('chartCanvas').getContext('2d');
+        window.myChart = new Chart(ctx, config);
+    }
+
+    function bindTableClickEvent() {
+        $('#dataTable tbody').on('click', 'td:first-child', function() {
+            const brandName = $(this).text();
+            renderChart(brandName);
+            $('#chartModal').modal('show');
+        });
+    }
+
     function destroyAndResetTable() {
         if ($.fn.DataTable.isDataTable('#dataTable')) {
             dataTable.destroy();
@@ -35,7 +79,10 @@ $(document).ready(function() {
                 }
             },
             columns: [
-                { data: "Brnd_Name" },
+                {
+                    data: "Brnd_Name",
+                    className: "table-clickable-cell"
+                },
                 { data: "Gnrc_Name" },
                 { data: "Year" },
                 { data: "TotalSpending" },
@@ -80,6 +127,7 @@ $(document).ready(function() {
 
     // Initialize the default table
     initializeDefaultTable();
+    bindTableClickEvent();
 
     // Event listener for the checkbox
     $('#spendingGeneric').on('change', function() {
@@ -88,6 +136,7 @@ $(document).ready(function() {
         } else {
             initializeDefaultTable();
         }
+        bindTableClickEvent();
     });
 
     // Reload data on dropdown change
--- Pages/medicareSpending.cfm
+++ Pages/medicareSpending.cfm
@@ -7,11 +7,19 @@
     <title>Medicare Spending</title>
 
     <link rel="stylesheet" href="/CapstoneIT491/CSS/styles.css">
-    <script src="../JavaScript/script.js"></script>
+    <!---<script src="../JavaScript/script.js"></script>--->
 
-    <!---<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">--->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 
+    <!-- Bootstrap CSS and JS -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <!---<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>--->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+    <!-- Chart.js -->
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.css">
     <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.js"></script>
 
@@ -46,8 +54,22 @@
         </thead>
         <tbody></tbody>
     </table>
-
-
+    <!-- Bootstrap Modal for Chart Display -->
+    <div class="modal fade" id="chartModal" tabindex="-1" role="dialog" aria-labelledby="chartModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="chartModalLabel">Brand Details</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <canvas id="chartCanvas"></canvas>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 <br>
